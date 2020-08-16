117f2427b2774d3c3b7d52a75fff7eb2e378562d
Obaidur830 <bsse0830@iit.du.ac.bd>
2020-08-16 23:23:05 +0600

    data fetched in one array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {MatchDetails} from '../interfaces/match-details';
+import {MatchDetails} from './interfaces/match-details';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +10,31 @@ export class AppComponent {
   title = 'premierLeagueFifteenSixteen';
   url = 'https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json';
   obj: any;
-  matches: MatchDetails[];
-  constructor(private http: HttpClient) {
+  matches: MatchDetails[] = [];
+  match: MatchDetails;
+  // tslint:disable-next-line: align
+  constructor(private http: HttpClient){
     this.http.get(this.url).toPromise().then(data => {
       this.obj = data;
-      console.log(this.obj.rounds[0].matches[0].date);
+      // console.log(this.obj.rounds[0].matches[0].date);
+
+      // tslint:disable-next-line: forin
+      for (const i in this.obj.rounds) {
+       // tslint:disable-next-line: forin
+       for (const j in this.obj.rounds[i].matches){
+
+             this.match = {
+                date: this.obj.rounds[i].matches[j].date,
+                team1: this.obj.rounds[i].matches[j].team1,
+                team2: this.obj.rounds[i].matches[j].team2,
+                score1: this.obj.rounds[i].matches[j].score.ft[0],
+                score2: this.obj.rounds[i].matches[j].score.ft[1]
+
+              };
+             this.matches.push(this.match);
+        }
+      }
+      console.log(this.matches);
     });
 
   }
